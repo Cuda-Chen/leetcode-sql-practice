0cc7b1e40ea740a233c1375335f129b9256462f5
Cuda-Chen <clh960524@gmail.com>
2022-07-23 20:41:39 +0800

    Add MySQL solution of problem #1393

---
+++ 1393/solution.sql
@@ -0,0 +1,28 @@
+# Write your MySQL query statement below
+
+# temp table
+with temp1 as (
+    select
+        stock_name,
+        sum(price) as 'buy'
+    from Stocks
+    where operation = 'Buy'
+    group by stock_name
+), temp2 as (
+    select
+        stock_name,
+        sum(price) as 'sell'
+    from Stocks
+    where operation = 'Sell'
+    group by stock_name
+)
+
+select
+    t1.stock_name,
+    sell - buy as 'capital_gain_loss'
+from
+    temp1 as t1,
+    temp2 as t2
+where
+    t1.stock_name = t2.stock_name
+;
